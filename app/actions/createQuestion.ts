412d407cc5d91e06c7fa9bbb8606e7ab5f2319a1
dnchia <daniel.chia.95@gmail.com>
2016-12-20 11:38:24 +0100

    Updated CreateQuestionForm, created but not working.

--- app/actions/createQuestion.ts
+++ app/actions/createQuestion.ts
@@ -1,6 +1,48 @@
+import {Action, Dispatch} from "redux";
+import {GenericAction} from "./common";
 import {QuestionFormFields} from "../components/questions/questionForm";
+import {ApplicationState} from "../reducers/index";
+import {ThunkAction} from "redux-thunk";
+import {RestAdapter} from "../adapters/rest";
+
+export const QUESTION_CREATION_PENDING = 'QUESTION_CREATION_PENDING';
+export const QUESTION_CREATION_SUCCESS = 'QUESTION_CREATION_SUCCESS';
+export const QUESTION_CREATION_ERROR = 'QUESTION_CREATION_ERROR';
+export const QUESTION_CREATION_CLEAR = 'QUESTION_CREATION_CLEAR';
 
 
-export function createQuestion(answerData : QuestionFormFields) {
-    //TODO: Implement
+function questionCreation_pending() : Action {
+    return {type: QUESTION_CREATION_PENDING};
+}
+
+function questionCreation_success(data : any) : GenericAction {
+    return {
+        type: QUESTION_CREATION_SUCCESS,
+        payload: data
+    };
+}
+
+function questionCreation_failed(err : Error) : GenericAction {
+    return {
+      type : QUESTION_CREATION_ERROR,
+      error : err
+    };
+}
+
+export function questionCreation_clear() : Action {
+    return {
+        type : QUESTION_CREATION_CLEAR
+    }
+}
+
+export function questionCreation_createQuestion(questionData : QuestionFormFields) : ThunkAction<void, ApplicationState, void> {
+    // TODO: INSERT FINAL DIRECTION AND USER VALIDATION
+    const request = RestAdapter.post('/WOLOLO', questionData);
+
+    return (dispatch: Dispatch<ApplicationState>) => {
+        dispatch(questionCreation_pending());
+        request
+            .then((data) => dispatch(questionCreation_success(questionData)))
+            .catch((err) => dispatch(questionCreation_failed(err)));
+    };
 }
---
+++ app/reducers/reducer_questionCreation.ts
@@ -0,0 +1,37 @@
+import {Action} from "redux";
+import {GenericAction} from "../actions/common";
+import {
+    QUESTION_CREATION_PENDING, QUESTION_CREATION_SUCCESS, QUESTION_CREATION_ERROR,
+    QUESTION_CREATION_CLEAR
+} from "../actions/createQuestion";
+
+export interface QuestionCreationState {
+    creatingQuestion : boolean,
+    questionCreated : boolean,
+    error : {[field : string] : string}
+}
+
+const INITIAL_STATE : QuestionCreationState = {
+    creatingQuestion: false,
+    questionCreated : false,
+    error : undefined
+};
+
+export const QuestionCreationReducer = (state: QuestionCreationState = INITIAL_STATE, action : GenericAction) => {
+    switch (action.type) {
+        case QUESTION_CREATION_PENDING:
+            return {creatingQuestion : true, questionCreated : false, error : undefined};
+
+        case QUESTION_CREATION_SUCCESS:
+            return {creatingQuestion : false, questionCreated : true, error : undefined};
+
+        case QUESTION_CREATION_ERROR:
+            return {creatingQuesion : true, questionCreated : false, error : {question : 'empty-question', answerA : 'empty-answerA', answerB : 'empty-answerB', answerC : 'empty-answerC', answerD : 'empty-answerD', correctAnswer : 'not-selected'}};
+
+        case QUESTION_CREATION_CLEAR:
+            return INITIAL_STATE;
+
+        default:
+            return state;
+    }
+};
