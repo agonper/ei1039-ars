622f1f2c1a05d1f380dde3cc013c9f9e06990be6
Alberto Gonzalez Perez <al286290@uji.es>
2016-12-30 20:26:23 +0100

    Refactoring courses actions

--- app/actions/courses.ts
+++ app/actions/courses.ts
@@ -1,35 +1,22 @@
-import {Action, Dispatch} from "redux";
-import {GenericAction} from "./common";
-import {ApplicationState} from "../reducers/index";
-import {ThunkAction} from "redux-thunk";
-import {apolloClient} from "../adapters/graphql";
+import {performGraphQLQuery, performGraphQLMutation} from "./common";
 import gql from "graphql-tag/index";
 import {NewCourseData} from "../components/dashboard/add-course-modal";
 
-export const LIST_COURSES_PENDING = 'USER_SIGNUP_PENDING';
-export const LIST_COURSES_SUCCESS = 'USER_SIGNUP_SUCCESS';
-export const LIST_COURSES_ERROR = 'USER_SIGNUP_ERROR';
+export const LIST_COURSES_PENDING = 'LIST_COURSES_PENDING';
+export const LIST_COURSES_SUCCESS = 'LIST_COURSES_SUCCESS';
+export const LIST_COURSES_ERROR = 'LIST_COURSES_ERROR';
 
-function listCoursesPending(): Action{
-    return {
-        type: LIST_COURSES_PENDING
-    }
-}
+export const DISPLAY_COURSE_PENDING = 'DISPLAY_COURSE_PENDING';
+export const DISPLAY_COURSE_SUCCESS = 'DISPLAY_COURSE_SUCCESS';
+export const DISPLAY_COURSE_ERROR = 'DISPLAY_COURSE_ERROR';
 
+export const FETCH_COURSE_PENDING = 'FETCH_COURSE_PENDING';
+export const FETCH_COURSE_SUCCESS = 'FETCH_COURSE_SUCCESS';
+export const FETCH_COURSE_ERROR = 'FETCH_COURSE_ERROR';
 
-function listCoursesSuccess(data: any): GenericAction {
-    return {
-        type: LIST_COURSES_SUCCESS,
-        payload: data
-    }
-}
-
-function listCoursesFailed(err: any): GenericAction {
-    return {
-        type: LIST_COURSES_ERROR,
-        error: err
-    }
-}
+export const CREATE_COURSE_PENDING = 'CREATE_COURSE_PENDING';
+export const CREATE_COURSE_SUCCESS = 'CREATE_COURSE_SUCCESS';
+export const CREATE_COURSE_ERROR = 'CREATE_COURSE_ERROR';
 
 const UserCoursesQuery = gql`
  query {
@@ -48,40 +35,14 @@ const UserCoursesQuery = gql`
   }
 }`;
 
-export function fetchUserCourses(): ThunkAction<void, ApplicationState, void> {
-    const request = apolloClient.query({query: UserCoursesQuery});
-    return (dispatch: Dispatch<ApplicationState>) => {
-        dispatch(listCoursesPending());
-        request
-            .then((data) => {
-                dispatch(listCoursesSuccess(data));
-            }).catch((err) => dispatch(listCoursesFailed(err)));
-        return request;
-    }
-}
+export function fetchUserCourses() {
+    const actionTypes = {
+        pending: LIST_COURSES_PENDING,
+        success: LIST_COURSES_SUCCESS,
+        failure: LIST_COURSES_ERROR
+    };
 
-export const CREATE_COURSE_PENDING = 'CREATE_COURSE_PENDING';
-export const CREATE_COURSE_SUCCESS = 'CREATE_COURSE_SUCCESS';
-export const CREATE_COURSE_ERROR = 'CREATE_COURSE_ERROR';
-
-function createCoursePending(): Action {
-    return {
-        type: CREATE_COURSE_PENDING
-    }
-}
-
-function createCourseSuccess(data: any): GenericAction {
-    return {
-        type: CREATE_COURSE_SUCCESS,
-        payload: data
-    }
-}
-
-function createCourseFailed(error: any): GenericAction {
-    return {
-        type: CREATE_COURSE_ERROR,
-        error
-    }
+    return performGraphQLQuery({query: UserCoursesQuery}, actionTypes);
 }
 
 const CreateCourseMutation = gql`
@@ -96,39 +57,14 @@ const CreateCourseMutation = gql`
     }
  }`;
 
-export function createCourse(course: NewCourseData): ThunkAction<void, ApplicationState, void> {
-    const request = apolloClient.mutate({mutation: CreateCourseMutation, variables: {name: course.name}});
-    return (dispatch: Dispatch<ApplicationState>) => {
-        dispatch(createCoursePending());
-        request
-            .then((data) => dispatch(createCourseSuccess(data)))
-            .catch((err) => dispatch(createCourseFailed(err)));
-        return request;
-    }
-}
+export function createCourse(course: NewCourseData) {
+    const actionTypes = {
+        pending: CREATE_COURSE_PENDING,
+        success: CREATE_COURSE_SUCCESS,
+        failure: CREATE_COURSE_ERROR
+    };
 
-export const FETCH_COURSE_PENDING = 'FETCH_COURSE_PENDING';
-export const FETCH_COURSE_SUCCESS = 'FETCH_COURSE_SUCCESS';
-export const FETCH_COURSE_ERROR = 'FETCH_COURSE_ERROR';
-
-function fetchCoursePending(): Action {
-    return {
-        type: FETCH_COURSE_PENDING
-    }
-}
-
-function fetchCourseSuccess(data: any): GenericAction {
-    return {
-        type: FETCH_COURSE_SUCCESS,
-        payload: data
-    }
-}
-
-function fetchCourseFailed(err: any): GenericAction {
-    return {
-        type: FETCH_COURSE_ERROR,
-        payload: err
-    }
+    return performGraphQLMutation({mutation: CreateCourseMutation, variables: {name: course.name}}, actionTypes);
 }
 
 const FetchCourseQuery = gql`
@@ -143,39 +79,14 @@ const FetchCourseQuery = gql`
    }
  }`;
 
-export function fetchCourse(id: string): ThunkAction<void, ApplicationState, void> {
-    const request = apolloClient.query({query: FetchCourseQuery, variables: {id}});
-    return (dispatch: Dispatch<ApplicationState>) => {
-        dispatch(fetchCoursePending());
-        request
-            .then((data) => dispatch(fetchCourseSuccess(data)))
-            .catch((err) => dispatch(fetchCourseFailed(err)));
-        return request;
-    }
-}
+export function fetchCourse(id: string) {
+    const actionTypes = {
+        pending: FETCH_COURSE_PENDING,
+        success: FETCH_COURSE_SUCCESS,
+        failure: FETCH_COURSE_ERROR
+    };
 
-export const DISPLAY_COURSE_PENDING = 'DISPLAY_COURSE_PENDING';
-export const DISPLAY_COURSE_SUCCESS = 'DISPLAY_COURSE_SUCCESS';
-export const DISPLAY_COURSE_ERROR = 'DISPLAY_COURSE_ERROR';
-
-function displayCoursePending(): Action {
-    return {
-        type: DISPLAY_COURSE_PENDING
-    }
-}
-
-function displayCourseSuccess(data: any): GenericAction {
-    return {
-        type: DISPLAY_COURSE_SUCCESS,
-        payload: data
-    }
-}
-
-function displayCourseFailed(err: any): GenericAction {
-    return {
-        type: DISPLAY_COURSE_ERROR,
-        payload: err
-    }
+    return performGraphQLQuery({query: FetchCourseQuery, variables: {id}}, actionTypes);
 }
 
 const DisplayCourseQuery = gql`
@@ -196,13 +107,12 @@ const DisplayCourseQuery = gql`
    }
  }`;
 
-export function displayCourse(id: string): ThunkAction<void, ApplicationState, void> {
-    const request = apolloClient.query({query: DisplayCourseQuery, variables: {id}});
-    return (dispatch: Dispatch<ApplicationState>) => {
-        dispatch(displayCoursePending());
-        request
-            .then((data) => dispatch(displayCourseSuccess(data)))
-            .catch((err) => dispatch(displayCourseFailed(err)));
-        return request;
-    }
+export function displayCourse(id: string) {
+    const actionTypes = {
+        pending: DISPLAY_COURSE_PENDING,
+        success: DISPLAY_COURSE_SUCCESS,
+        failure: DISPLAY_COURSE_ERROR
+    };
+
+    return performGraphQLQuery({query: DisplayCourseQuery, variables: {id}}, actionTypes);
 }
